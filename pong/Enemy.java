108686fa4fa15e0b2378399192202e5b9c0ef824
Luis Octavio Galesso Seror <luisoctaviogalessoseror@protonmail.com>
2021-03-25 12:09:45 -0400

    update

---
+++ pong/Enemy.java
@@ -0,0 +1,35 @@
+package pong;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+public class Enemy {
+
+	public double x,y;
+	public int width,height;
+	public static double dificult = 0.21; 
+	
+	public Enemy(int x,int y) {
+		this.x = x;
+		this.y = y;
+		this.width = 2;
+		this.height = 30;
+	}
+	
+	public void tick() {
+		y+= (Game.ball.y - y - 6) * dificult;
+		if(y + height > Game.HEIGHT ) {
+			y = Game.HEIGHT - height;
+		}
+		else if(y < 25) {
+			y = 25;
+		}
+	}
+	
+	public void render(Graphics g) {
+		g.setColor(Color.white);
+		g.fillRect((int)x,(int)y,width,height);
+	}
+	
+}
---
+++ pong/Player.java
@@ -0,0 +1,46 @@
+package pong;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Player {
+
+	
+	public boolean up,down;
+	
+	public int x,y;
+	
+	public int width,height;
+	
+	public Player(int x, int y) {
+		this.x = x;
+		this.y = y;
+		this.width = 2;
+		this.height = 25;
+	}
+	
+	public void tick(){
+		if(up)
+		{
+			y-=6;
+		}
+		else if(down) {
+			y+=6;
+		}
+		
+		if(y + height > Game.HEIGHT)
+		{
+			y = Game.HEIGHT - height;
+		}
+		
+		else if(y < 25) {
+			y = 25;
+		}
+	}
+	
+	public void render(Graphics g){
+		g.setColor(Color.white);
+		g.fillRect(x,y,width,height);
+	}
+	
+}
